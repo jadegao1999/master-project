e471f3d296cc024f1d4b4a62e531e9517f1cb26d
Jianan Fang <fjn19971007@outlook.com>
2021-03-03 23:05:41 -0800

    init commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
 import React from "react"
+import { Helmet } from "react-helmet"
+import App from "../components/app";
+
+import "./index.css"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <Helmet>
+        <title>Taylor's Tidy Trees</title>
+        <link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+      </Helmet>
+      <div className="body">
+        <App/>
+      </div>
+    </>
+  )
 }
