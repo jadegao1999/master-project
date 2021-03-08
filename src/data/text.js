b4f7aef9c58ae07291a389ea3505cd3709e6ceca
jadegao1999 <43734738+jadegao1999@users.noreply.github.com>
2021-03-07 19:00:21 -0800

    Update text.js

--- src/data/text.js
+++ src/data/text.js
@@ -2,12 +2,18 @@
 import { Dialog, Narrative } from "../models/text_content";
 
 const textList = [
-  new Narrative('0', 'Tonight, you and your younger sister, Baoluo, are going to a blind date at Paramount, the biggest night club in 1940, Shanghai. '),
-  new Narrative('1', 'The party will start in an hour. You pick up the old cheongsam(a traditional Chinese dress) you just ironed, stand in front of the mirror and imagine wearing it.'),
-  new Narrative('2', 'It suddenly reminds you that you wore it seven years ago, also on a blind date, where you met your unfaithful, violent ex-husband.'),
-  new Narrative('3', 'Although it may be unlucky, it\'s the only formal wear you have left and you have no budget for a new dress.'),
-  new Dialog('4', '"Liusu, what are you doing?! "', null, 'liusu'),
-  new Narrative('6', 'You hear your mother, Lady Bai, calling your name from the other side of the house, "come over here and help your sister!" '),
+  new Narrative('0', 'Tonight, you and your younger sister, Baoluo, are going on a blind date at Paramount, the biggest nightclub in Shanghai in 1940. The party will start in an hour.'),
+  new Narrative('1', '<<< Click on the cheongsam on the left to wear it on.'),
+  new Narrative('2', 'You pick up the old cheongsam (a traditional Chinese dress) you just ironed and wear it on.'),
+  new Narrative('3', 'It suddenly reminds you that you wore it seven years ago, also on a blind date, where you met your unfaithful, violent ex-husband.'),
+  new Narrative('4', 'Although it may be unlucky, it\'s the only formal wear you have left, and you have no budget for a new dress.')
+  new Narrative('5', 'Maybe putting on your favourite pearl earrings will add some luck.')
+  new Narrative('6', '<<< Click on the pearl earrings on the left to put them on.')
+  new Dialog('7', '"Liusu, what are you doing?! "', null, 'door'),
+  new Narrative('8', 'You hear your mother, Lady Bai, calling your name from the other side of the house.'),
+  new Dialog('9', '"come over here and help your sister!" ', null, 'door'),
+  new Narrative('10', 'What? They still need more help? Everyone in the house is in that room dressing up Baoluo, and they almost upturn the house!'),
+  new Narrative('11', 'What a glittering beauty! You can hardly recognize Baoluo. And the delicate cheongsam and jewelry on her – for sure you haven\'t seen such luxuries in the decayed Bai family for years.'),
 ];
 
 const textMap = {};
