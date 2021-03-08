3c7d814c910e1f044fc0234ec2d35fcc0f7d6132
jadegao1999 <43734738+jadegao1999@users.noreply.github.com>
2021-03-08 01:14:55 -0800

    Update images.js

--- src/data/images.js
+++ src/data/images.js
@@ -8,6 +8,7 @@ import liusu_green from '../assets/images/liusu_green.png';
 import liusu_yellow from '../assets/images/liusu_yellow.png';
 import yellow_dress from '../assets/images/yellow_dress.png';
 import yiyuan_card from '../assets/images/yiyuan_card.png';
+import old_lady_bai from '../assets/images/old_lady_bai.png';
 import { Person, Item } from '../models/picture';
 
 export const imagesMap = {
@@ -21,6 +22,7 @@ export const imagesMap = {
   liusu_yellow: new Person('liusu_yellow', 'liusu_yellow', liusu_yellow),
   yellow_dress: new Item('yellow_dress', 'yellow_dress', yellow_dress),
   yiyuan_card: new Item('yiyuan_card', 'yiyuan_card', yiyuan_card),
+  old_lady_bai: new Item('old_lady_bai', 'old_lady_bai', old_lady_bai),
   
 
   
