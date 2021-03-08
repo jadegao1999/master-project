8d33114968ec560b9162b2345fafa7ca188cb75b
Jianan Fang <fjn19971007@outlook.com>
2021-03-07 22:46:30 -0800

    add persons field to TextContent

--- src/models/text_content.js
+++ src/models/text_content.js
@@ -1,25 +1,36 @@
-
+import React from 'react';
 
 class TextContent {
-  constructor(id, text, onClickActionList = []) {
+  constructor(id, text, onClickActionList = [], persons = []) {
     this.id = id;
-    // words wrapped with $1{} will be converted to a clickable button and
+    // words wrapped with ${} will be converted to a clickable button and
     // binds with the callback function onClickList[1].
-    // For example, $0{show card} binds to onClickList[0].
+    // For example, ${show card} binds to onClickList[0].
     this.text = text;
     this.onClickActionList = onClickActionList;
+
+    this.persons = persons;
+  }
+
+  renderText() {
+    var res = this.text.slice();
+    var matches = this.text.match(/\${.*}/);
+    for (let i = 0; i < matches.length; i++) {
+      var word = matches[i].slice(1)
+      res.replace(matches[0], <span></span>)
+    }
   }
 }
 
 class Narrative extends TextContent {
-  constructor(id, text, onClickActionList = []) {
-    super(id, text, onClickActionList);
+  constructor(id, text, onClickActionList = [], persons = []) {
+    super(id, text, onClickActionList, persons);
   }
 }
 
 class Dialog extends TextContent {
-  constructor(id, text, onClickActionList = [], speakerId = null) {
-    super(id, text, onClickActionList);
+  constructor(id, text, onClickActionList = [], speakerId = null, persons = []) {
+    super(id, text, onClickActionList, persons);
     this.speakerId = speakerId;
   }
 }
