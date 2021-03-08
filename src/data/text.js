391742dc0f54a27ca67258a212431f822e88ac86
jadegao1999 <43734738+jadegao1999@users.noreply.github.com>
2021-03-08 00:37:46 -0800

    Update text.js

--- src/data/text.js
+++ src/data/text.js
@@ -3,31 +3,40 @@ import { Dialog, Narrative } from "../models/text_content";
 
 const textList = [
   // prolog
-  new Narrative('0', 'Tonight, you and your younger sister, ${Baoluo}, are going on a blind date at Paramount, the biggest nightclub in Shanghai in 1940. The party will start in an hour.'),
+  new Narrative('0', 'Tonight, you and your younger sister, ${Baoluo}, are going on a blind date at Paramount, the biggest nightclub in Shanghai in 1940. The party will start in an hour.', ['baoluo_card'], [null, 'liusu']),
   // baoluo - baoluo card pops up on left
-  new Narrative('1', '<<< Click on the cheongsam on the left to wear it on.'),
-  new Narrative('2', 'You pick up the old cheongsam (a traditional Chinese dress) you just ironed and wear it on.'),
-  new Narrative('3', 'It suddenly reminds you that you wore it seven years ago, also on a blind date, where you met your unfaithful, violent ex-husband.'),
+  new Narrative('1', '<<< Click on the cheongsam on the left to wear it on.', null, ['green_dress', 'liusu']),
+  new Narrative('2', 'You pick up the old cheongsam (a traditional Chinese dress) you just ironed and wear it on.', null, ['yellow_dress', 'liusu_green']),
+  new Narrative('3', 'It suddenly reminds you that you wore it seven years ago, also on a blind date, where you met your unfaithful, violent ${ex_husband}.', ['yiyuan_card'], [null, 'liusu_green']),
   new Narrative('4', 'Although it may be unlucky, it\'s the only formal wear you have left, and you have no budget for a new dress.'),
-  new Narrative('5', 'Maybe putting on your favourite pearl earrings will add some luck.'),
+  new Narrative('5', 'Maybe putting on your favourite pearl earrings will add some luck.', null, ['earrings', 'liusu_green']),
   new Narrative('6', '<<< Click on the pearl earrings on the left to put them on.'),
-  new Dialog('7', '"Liusu, what are you doing?! "', null, 'door'),
+  new Dialog('7', '"Liusu, what are you doing?! "', null, 'door', ['door', 'liusu_earrings']),
   new Narrative('8', 'You hear your mother, Lady Bai, calling your name from the other side of the house.'),
   new Dialog('9', '"come over here and help your sister!" ', null, 'door'),
   new Narrative('10', 'What? They still need more help? Everyone in the house is in that room dressing up Baoluo, and they almost upturn the house!'),
   // button here: walk to your mother's room
-  new Narrative('11', 'What a glittering beauty! You can hardly recognize Baoluo. And the delicate cheongsam and jewelry on her – for sure you haven\'t seen such luxuries in the decayed Bai family for years.'),
+  new Narrative('11', 'What a glittering beauty! You can hardly recognize Baoluo. And the delicate cheongsam and jewelry on her – for sure you haven\'t seen such luxuries in the decayed Bai family for years.', null, ['baoluo', 'liusu_earrings']),
   new Dialog('12', '"Mom forced our sister-in-law to hand over the fine silk she just got as a birthday gift to make me this cheongsam..." ', null, 'baoluo'),
   new Narrative('13', 'Seeing you with a puzzled face, Baoluo explains,'),
   new Dialog('14', '"and she pawned her very own old furs to buy me these bracelets and rings..." ', null, 'baoluo'),
   new Narrative('15', 'No one ever cared about what you wear. You don\'t hate Baoluo, it\'s just two daughters in the same house, but one gets lots of attention, and the other gets cold silence, and sadly you are used to that.'),
-  new Dialog('16', '"There is no time to talk nonsense to your sister now. Liusu, your sister needs a pair of good earrings. I remember you still have a pair of pearl ones."', null, 'old lady bai'),
+  new Dialog('16', '"There is no time to talk nonsense to your sister now. Liusu, your sister needs a pair of good earrings. I remember you still have a pair of pearl ones."', null, 'old lady bai', ['old_lady_bai', 'liusu_earrings']),
   new Narrative('17', 'She ties a final piece of green jade around Baoluo\'s neck and turns to you.'),
-  new Dialog('18', '"Oh, you are wearing them! See, I thought it\'s a good match for the color of Baoluo\'s cheongsum."', null, 'old lady bai'),
+  new Dialog('18', '"Oh, you are wearing them! See, I thought it\'s a good match for the color of Baoluo\'s cheongsum."', null, 'old_lady_bai'),
   new Narrative('19', 'Hah! But, those are your favorite ones, and you are wearing them NOW!'),
   // button here: choice1 - take them off and give them to Baoluo
-  new Dialog('20', '"Here."', null, 'liusu-green'),
-  new Narrative('21', 'Hah! But, those are your favorite ones, and you are wearing them NOW!'),
+  new Dialog('20', '"Here."', null, 'liusu', ['baoluo','liusu_green']),
+  new Narrative('21', 'You take off the earrings and hand them over to Baoluo.', null, ['baoluo_earrings', 'liusu_green']),
+  new Dialog('22', '"Thanks Liusu! I told mom that this is your favorite pair, but she ..."', null, 'baoluo_earrings'),
+  new Narrative('23', 'Baoluo seems sorry.', null),
+  new Dialog('24', '"I\'m doing this for you!"', null, 'old_aldy_bai', ['old_lady_bai', 'liusu_green']),
+  new Narrative('25', 'Lady Bai interrupts again.', null),
+  new Dialog('26', '"Mothers in Shanghai are pushing their daughters to the party. You have to stand out.  It\'s hard to find a perfect marriage candidate like Liuyuan."', null, 'old_aldy_bai'),
+  new Dialog('27', '"I heard that his family owns a successful business in London, and he is the ONLY SON!!"', null, 'old_lady_bai'),
+  new Dialog('28', '"Aside from that..."', null, 'sister_in_law'),
+  
+  
   
   // choice2 - insist on wearing them yourslef
 
