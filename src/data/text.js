3be0e636fa49bb17ebb1832dae8209eb709abe31
Jade Gao <jadegao1999@gmail.com>
2021-03-10 23:08:08 -0800

    Update text.js

--- src/data/text.js
+++ src/data/text.js
@@ -9,8 +9,8 @@ const textList = [
   new Narrative('2', 'You pick up the old cheongsam (a traditional Chinese dress) you just ironed and wear it on.', null, ['yellow_dress', 'liusu_green']),
   new Narrative('3', 'It suddenly reminds you that you wore it seven years ago, also on a blind date, where you met your unfaithful, violent ex_husband.', null, [null, 'liusu_green']),
   new Narrative('4', 'Although it may be unlucky, it\'s the only formal wear you have left, and you have no budget for a new dress.'),
-  new Narrative('5', 'Maybe putting on your favourite pearl earrings will add some luck.', null, ['earrings', 'liusu_green']),
-  new Action('6', '<<< Click on the pearl earrings on the left to put them on.'),
+  new Narrative('5', 'Maybe putting on your favourite pearl earrings will add some luck.', null),
+  new Action('6', '<<< Click on the pearl earrings on the left to put them on.', ['earrings', 'liusu_green']),
   new Dialog('7', '"Liusu, what are you doing?! "', null, 'door', ['door', 'liusu_earrings']),
   new Narrative('8', 'You hear your mother, Lady Bai, calling your name from the other side of the house.', null),
   new Dialog('9', '"come over here and help your sister!" ', null, 'door'),
@@ -42,7 +42,7 @@ const textList = [
   new Narrative('33', 'After all, who would take your remarriage seriously? And, is a remarriage that important to you?', null),
   new Narrative('34', 'At the appointed time, all family members cram into a taxi-cab.', null, ['taxi', 'liusu_green']),
   //button here: Arrive at Paramount (GiCarWheel)
-  
+
 
 
   
